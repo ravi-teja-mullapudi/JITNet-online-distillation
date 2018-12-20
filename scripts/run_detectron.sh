c2a162a116acc4e7a66c04a72750b8dcaa20a892
Ravi <rmullapu@cs.cmu.edu>
2018-12-20 14:39:01 -0500

    Adding script to run detectron on video stream

---
+++ scripts/run_detectron.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=$3 python Detectron.pytorch/tools/infer_video_stream.py --dataset coco --cfg=Detectron.pytorch/configs/baselines/e2e_mask_rcnn_X-152-32x8d-FPN-IN5k_1.44x_no_aug.yaml --load_detectron=./pretrained/model_large_final.pkl --input_video_path=$1 --output_path=$2
